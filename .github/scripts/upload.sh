1e021ae28de5e565f26a2e16c5da40f07592e0f5
Joshua Schoep <joshuaschoep@gmail.com>
2024-01-22 13:57:23 -0700

    monorepo shift

--- .github/scripts/upload.sh
+++ .github/scripts/upload.sh
@@ -1,1 +1,2 @@
-aws s3 cp $AWS_TEMPLATE_FILE s3://$AWS_S3_BUCKET/$AWS_TEMPLATE_FILE
+aws s3 sync templates/common s3://$AWS_S3_BUCKET/common --delete
+aws s3 sync templates/deployed s3://$AWS_S3_BUCKET/deployed --delete
